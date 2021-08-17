622bb1e70afc5ba77525ed0c8467c80be1bea853
Mifahul Khoir <miftahul@gredu.asia>
2021-08-17 20:58:52 +0700

    fix: optimize rendering

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,45 +1,40 @@
-// /**
-//  * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
-//  *
-//  * See: https://www.gatsbyjs.com/docs/ssr-apis/
-//  */
+/**
+ * Implement Gatsby's SSR (Server Side Rendering) APIs in this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/ssr-apis/
+ */
 
-// // You can delete this file if you're not using it
-// import React from "react"
-// import { withPrefix } from "gatsby-link"
+// You can delete this file if you're not using it
+import React from "react"
+import { withPrefix } from "gatsby-link"
 
-// export const onRenderBody = ({ setPostBodyComponents }) => {
-//   setPostBodyComponents([
-//     <script
-//       key="jquery"
-//       src={withPrefix("js/jquery.min.js")}
-//       type="text/javascript"
-//       defer
-//     />,
-//     <script
-//       key="bootstrap"
-//       src={withPrefix("js/bootstrap.min.js")}
-//       type="text/javascript"
-//       defer
-//     />,
-//     <script
-//       key="circleProgress"
-//       src={withPrefix("js/circle-progress.min.js")}
-//       type="text/javascript"
-//       defer
-//     />,
-//     <script
-//       key="jqueryPluginCollection"
-//       src={withPrefix("js/jquery-plugin-collection.js")}
-//       type="text/javascript"
-//       defer
-//     />,
+export const onRenderBody = ({ setPostBodyComponents }) => {
+  setPostBodyComponents([
+    <script
+      key="jquery"
+      src={withPrefix("js/jquery.min.js")}
+      type="text/javascript"
+    />,
+    <script
+      key="bootstrap"
+      src={withPrefix("js/bootstrap.min.js")}
+      type="text/javascript"
+    />,
+    <script
+      key="circleProgress"
+      src={withPrefix("js/circle-progress.min.js")}
+      type="text/javascript"
+    />,
+    <script
+      key="jqueryPluginCollection"
+      src={withPrefix("js/jquery-plugin-collection.js")}
+      type="text/javascript"
+    />,
 
-//     // <script
-//     //   key="etet"
-//     //   src={withPrefix("js/script.js")}
-//     //   type="text/javascript"
-//     //   defer
-//     // />,
-//   ])
-// }
+    <script
+      key="etet"
+      src={withPrefix("js/script.js")}
+      type="text/javascript"
+    />,
+  ])
+}
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,15 +10,15 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql, withPrefix } from "gatsby"
 
 import Header from "./header"
-import useScript from "../hooks/useScript"
+// import useScript from "../hooks/useScript"
 import Footer from "./footer"
 
 const Layout = ({ children }) => {
-  useScript(withPrefix("js/jquery.min.js"))
-  useScript(withPrefix("js/bootstrap.min.js"))
-  useScript(withPrefix("js/circle-progress.min.js"))
-  useScript(withPrefix("js/jquery-plugin-collection.js"))
-  useScript(withPrefix("/js/script.js"))
+  // useScript(withPrefix("js/jquery.min.js"))
+  // useScript(withPrefix("js/bootstrap.min.js"))
+  // useScript(withPrefix("js/circle-progress.min.js"))
+  // useScript(withPrefix("js/jquery-plugin-collection.js"))
+  // useScript(withPrefix("/js/script.js"))
 
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
